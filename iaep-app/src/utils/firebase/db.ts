c529a929a18edf991d37cff1e41d83505771409e
Nama Kamu <emailkamu@gmail.com>
2026-07-24 01:28:24 +0700

    fix: set getFirestore return type to any to prevent db possibly null build errors

--- iaep-app/src/utils/firebase/db.ts
+++ iaep-app/src/utils/firebase/db.ts
@@ -1,8 +1,9 @@
 import { getFirebaseAdmin } from './server';
 
-export function getFirestore() {
+export function getFirestore(): any {
   const admin = getFirebaseAdmin();
   if (!admin) return null;
   return admin.firestore();
 }
+
 
